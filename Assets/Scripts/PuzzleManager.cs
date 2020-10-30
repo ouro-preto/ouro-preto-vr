7e1b7b73b3d716eb0240ec51ca1c02f929ef9779
Daniel Piassi <dandampi@gmail.com>
2020-10-30 13:57:27 -0300

    Puzzles : TreasureHunt

---
+++ Assets/Scripts/PuzzleManager.cs
@@ -0,0 +1,72 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class PuzzleManager : MonoBehaviour
+{
+    const string k_FolderName = "TreasureHunt";
+
+    [Header("UI")]
+
+    [SerializeField]
+    private GameObject m_Panel;
+
+    [SerializeField]
+    private Text m_NameText;
+
+    [SerializeField]
+    private Text m_DescriptionText;
+
+    public static PuzzleManager Instance { get; private set; }
+
+    private void Awake()
+    {
+        if (Instance == null)
+            Instance = this;
+        else if (Instance != this)
+            Destroy(gameObject);
+    }
+
+    private void Start()
+    {
+        var data = LoadJson($"{k_FolderName}/Data");
+        Debug.Log(JsonUtility.ToJson(data, true));
+
+        foreach (var x in data.items)
+        {
+            var prefab = Resources.Load($"{k_FolderName}/{x.prefab}") as GameObject;
+            prefab.AddComponent<Treasure>();
+
+            var instance = Instantiate(prefab, x.position, Quaternion.identity);
+            instance.GetComponent<Treasure>().Setup(x.name, x.description);
+        }
+    }
+
+    public void PickUp(Treasure treasure)
+    {
+        m_NameText.text = treasure.Name;
+        m_DescriptionText.text = treasure.Description;
+        m_Panel.SetActive(true);
+    }
+
+    private static Data LoadJson(string filePath)
+    {
+        TextAsset targetFile = Resources.Load<TextAsset>(filePath);
+        return JsonUtility.FromJson<Data>(targetFile.text);
+    }
+
+    [System.Serializable]
+    private struct Item
+    {
+        public string prefab;
+        public Vector3 position;
+        public string name;
+        public string description;
+    }
+
+    [System.Serializable]
+    private struct Data
+    {
+        public Item[] items;
+        public string[] prefabs;
+    }
+}
---
+++ Assets/Scripts/Treasure.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using Valve.VR.InteractionSystem;
+
+[RequireComponent(typeof(Throwable))]
+[RequireComponent(typeof(VelocityEstimator))]
+public class Treasure : MonoBehaviour
+{
+    private Throwable m_Throwable;
+
+    public string Name { get; private set; }
+
+    public string Description { get; private set; }
+
+    private void Start()
+    {
+        m_Throwable = GetComponent<Throwable>();
+        m_Throwable.onPickUp.AddListener(PickUp);
+    }
+
+    private void PickUp()
+    {
+        PuzzleManager.Instance.PickUp(this);
+    }
+
+    public void Setup(string name, string description)
+    {
+        Name = name;
+        Description = description;
+    }
+}
