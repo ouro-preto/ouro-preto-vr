0958d3372d6f47466d115249a0e9986e44009497
Daniel Piassi <dandampi@gmail.com>
2020-11-19 20:13:52 -0300

    polytope studio medieval characters downloaded from asset store

---
+++ Assets/Scripts/NonPlayableCharacter.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+public class NonPlayableCharacter : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject[] m_Prefabs;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        var i = Random.Range(0, m_Prefabs.Length);
+        var gameObject = Instantiate(m_Prefabs[i], transform.position, Quaternion.identity, transform);
+        gameObject.transform.LookAt(Vector3.zero);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        print("dfuasdfasd");
+    }
+}
