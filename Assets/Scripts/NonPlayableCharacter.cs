6335e917e22c86663bfc03e7dcb0ca5d09258122
Daniel Piassi <dandampi@gmail.com>
2020-11-23 16:31:21 -0300

    folders organized, some attempts on text/dialogue mechanics

--- Assets/Scripts/NonPlayableCharacter.cs
+++ Assets/Scripts/NonPlayableCharacter.cs
@@ -1,20 +1,50 @@
-﻿using UnityEngine;
+﻿using System.Collections;
+using UnityEngine;
 
 public class NonPlayableCharacter : MonoBehaviour
 {
     [SerializeField]
-    private GameObject[] m_Prefabs;
+    private GameObject[] m_Prefabs = null;
+
+    [SerializeField, TextArea]
+    private string[] m_Text = null;
+
+    private TextMesh m_TextMesh = null;
+    private Coroutine m_Coroutine = null;
 
     // Start is called before the first frame update
-    void Start()
+    private void Start()
     {
         var i = Random.Range(0, m_Prefabs.Length);
-        var gameObject = Instantiate(m_Prefabs[i], transform.position, Quaternion.identity, transform);
-        gameObject.transform.LookAt(Vector3.zero);
+        Instantiate(m_Prefabs[i], transform.position, Quaternion.identity, transform);
+        transform.LookAt(Vector3.zero);
+
+        m_TextMesh = GetComponentInChildren<TextMesh>();
+        m_TextMesh.text = m_Text.Length == 0 ? "" : m_Text[0];
     }
 
     private void OnTriggerEnter(Collider other)
     {
-        print("dfuasdfasd");
+        if (other.CompareTag("Player"))
+        {
+            m_Coroutine = StartCoroutine(Play());
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            StopCoroutine(m_Coroutine);
+        }
+    }
+
+    private IEnumerator Play()
+    {
+        for (int i = 1; i < m_Text.Length; i++)
+        {
+            m_TextMesh.text = m_Text[i];
+            yield return new WaitForSeconds(2f);
+        }
     }
 }
